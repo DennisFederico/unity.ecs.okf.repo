0905e93458f8cdf4e61993c3ddf4b3a6796cee33
Dennis Federico <dennisfederico@gmail.com>
2026-09-22 16:24:49 +0200

    Added server jar while its patched on source

--- start_kiso-okf.ps1
+++ start_kiso-okf.ps1
@@ -1,1 +1,2 @@
-kiso-mcp-server.exe -s ecs-okf -H 127.0.0.1 -p 61080
+#kiso-mcp-server.exe -s ecs-okf -H 0.0.0.0 -p 61080
+java --add-modules jdk.incubator.vector -jar .\kiso-mcp-server.jar -s ecs-okf -H 0.0.0.0 -p 61080
---
+++ start_kiso-okf.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+#kiso-mcp-server.exe -s ecs-okf -H 0.0.0.0 -p 61080
+java --add-modules jdk.incubator.vector -jar .\kiso-mcp-server.jar -s ecs-okf -H 0.0.0.0 -p 61080
